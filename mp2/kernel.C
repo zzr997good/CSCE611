eda62989b838c10fe234d48bd3c121065cf3b6db
Zhiren ZHOU <zzr997good@tamu.edu>
2022-09-18 15:49:38 -0500

    Test->Process pool tested successfully

--- mp2/kernel.C
+++ mp2/kernel.C
@@ -74,7 +74,7 @@ int main() {
 
     /* ---- PROCESS POOL -- */
 
-/*
+/* */
     unsigned long n_info_frames = ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
 
     unsigned long process_mem_pool_info_frame = kernel_mem_pool.get_frames(n_info_frames);
@@ -84,7 +84,7 @@ int main() {
                                    process_mem_pool_info_frame);
     
     process_mem_pool.mark_inaccessible(MEM_HOLE_START_FRAME, MEM_HOLE_SIZE);
-*/
+/* */
     /* -- MOST OF WHAT WE NEED IS SETUP. THE KERNEL CAN START. */
 
     Console::puts("Hello World!\n");
@@ -94,7 +94,7 @@ int main() {
     test_memory(&kernel_mem_pool, 32);
 
     /* ---- Add code here to test the frame pool implementation. */
-    
+    test_memory(&process_mem_pool, 50);
     /* -- NOW LOOP FOREVER */
     Console::puts("Testing is DONE. We will do nothing forever\n");
     Console::puts("Feel free to turn off the machine now.\n");

60429ef4d03a0b3f7bda16b1ad6f1f27d4b65e6f
Zhiren Zhou <zzr997good@tamu.edu>
2022-11-06 14:52:10 -0600

    Debugging...

--- mp5/thread.C
+++ mp5/thread.C
@@ -92,7 +92,7 @@ static void thread_shutdown() {
 
 static void thread_start() {
      /* This function is used to release the thread for execution in the ready queue. */
-    
+    if(!Machine::interrupts_enabled()) Machine::enable_interrupts();
      /* We need to add code, but it is probably nothing more than enabling interrupts. */
 }
 

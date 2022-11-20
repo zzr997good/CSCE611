4fdd7fe1f6a2447251ad5c36339252157aaeec1e
Zhiren Zhou <zzr997good@tamu.edu>
2022-11-20 14:58:13 -0600

    Debugging

--- mp6/mirrored_disk.C
+++ mp6/mirrored_disk.C
@@ -1,10 +1,10 @@
 /*
      File        : mirrored_disk.c
 
-     Author      : 
-     Modified    : 
+     Author      :
+     Modified    :
 
-     Description : 
+     Description :
 
 */
 
@@ -12,7 +12,7 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -24,24 +24,23 @@
 #include "mirrored_disk.H"
 #include "scheduler.H"
 #include "mem_pool.H"
-#include "thread.H" 
+#include "thread.H"
 
-extern MemPool * MEMORY_POOL;
-extern Scheduler * SYSTEM_SCHEDULER;
+extern MemPool *MEMORY_POOL;
+extern Scheduler *SYSTEM_SCHEDULER;
 /*--------------------------------------------------------------------------*/
 /* MIRRORED DISK */
 /*--------------------------------------------------------------------------*/
 
-MirroredDisk::MirroredDisk(DISK_ID _disk_id, unsigned int _size) 
-  : SimpleDisk(_disk_id, _size) {
-  
-  //Creastes 2 instances of the simple_disk
+MirroredDisk::MirroredDisk(DISK_ID _disk_id, unsigned int _size)
+    : SimpleDisk(_disk_id, _size)
+{
+
+  // Creastes 2 instances of the simple_disk
   master = new SimpleDisk(DISK_ID::MASTER, _size);
   dependent = new SimpleDisk(DISK_ID::DEPENDENT, _size);
   Console::puts("Constructed Derived MirroredDisk.\n");
 }
-
-
 
 /*--------------------------------------------------------------------------*/
 /* DISK FUNCTIONS */
@@ -51,129 +50,72 @@ void MirroredDisk::issue_mirrored_operation(DISK_ID _disk_id, DISK_OPERATION _op
   Machine::outportb(0x1F1, 0x00); /* send NULL to port 0x1F1         */
   Machine::outportb(0x1F2, 0x01); /* send sector count to port 0X1F2 */
   Machine::outportb(0x1F3, (unsigned char)_block_no);
-                         /* send low 8 bits of block number */
+  /* send low 8 bits of block number */
   Machine::outportb(0x1F4, (unsigned char)(_block_no >> 8));
-                         /* send next 8 bits of block number */
+  /* send next 8 bits of block number */
   Machine::outportb(0x1F5, (unsigned char)(_block_no >> 16));
-                         /* send next 8 bits of block number */
+  /* send next 8 bits of block number */
   unsigned int disk_no = _disk_id == DISK_ID::MASTER ? 0 : 1;
-  Machine::outportb(0x1F6, ((unsigned char)(_block_no >> 24)&0x0F) | 0xE0 | (disk_no << 4));
-                         /* send drive indicator, some bits, 
-                            highest 4 bits of block no */
+  Machine::outportb(0x1F6, ((unsigned char)(_block_no >> 24) & 0x0F) | 0xE0 | (disk_no << 4));
+  /* send drive indicator, some bits,
+     highest 4 bits of block no */
 
   Machine::outportb(0x1F7, (_op == DISK_OPERATION::READ) ? 0x20 : 0x30);
 }
 
+void MirroredDisk::read(unsigned long _block_no, unsigned char *_buf)
+{
+  Console::puts("Issuing mirrored operation\n ");
 
-void MirroredDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  Console::puts("Issuing mirrored operation\n "); 
-  
-  issue_mirrored_operation(DISK_ID::MASTER, DISK_OPERATION::READ, _block_no);      // IO command to the master
-  issue_mirrored_operation(DISK_ID::DEPENDENT,DISK_OPERATION::READ, _block_no);    // IO command to the dependent
-  while(!master->is_ready()&&!dependent->is_ready()){             //Both two disks are not ready for reading
-        SYSTEM_SCHEDULER->resume(Thread::CurrentThread());              //Add the thread to teh ready queue again
-        Console::puts("Device is not ready, voluntarily yielding thread\n ");  
-        SYSTEM_SCHEDULER->yield();
+  issue_mirrored_operation(DISK_ID::MASTER, DISK_OPERATION::READ, _block_no);    // IO command to the master
+  issue_mirrored_operation(DISK_ID::DEPENDENT, DISK_OPERATION::READ, _block_no); // IO command to the dependent
+  while (!master->is_ready() && !dependent->is_ready())
+  {                                                    // Both two disks are not ready for reading
+    SYSTEM_SCHEDULER->resume(Thread::CurrentThread()); // Add the thread to teh ready queue again
+    Console::puts("Device is not ready, voluntarily yielding thread\n ");
+    SYSTEM_SCHEDULER->yield();
   }
   int i;
   unsigned short tmpw;
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < 256; i++)
+  {
     tmpw = Machine::inportw(0x1F0);
-    _buf[i*2]   = (unsigned char)tmpw;
-    _buf[i*2+1] = (unsigned char)(tmpw >> 8);
+    _buf[i * 2] = (unsigned char)tmpw;
+    _buf[i * 2 + 1] = (unsigned char)(tmpw >> 8);
   }
 }
 
-
-void MirroredDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  
-  /*Saving the io request*/
-  // request->op = DISK_OPERATION::WRITE;
-  // request->rw_block_no = _block_no;
-  // request->buf = _buf;
-  
-  Console::puts("Issuing mirrored operation\n "); 
-  issue_mirrored_operation(DISK_ID::MASTER, DISK_OPERATION::WRITE, _block_no);      // IO command to the master
-  issue_mirrored_operation(DISK_ID::DEPENDENT,DISK_OPERATION::WRITE, _block_no);    // IO command to the dependent
-  while(!master->is_ready()&&!dependent->is_ready()){             //Both two disks are not ready for reading
-        SYSTEM_SCHEDULER->resume(Thread::CurrentThread());              //Add the thread to teh ready queue again
-        Console::puts("Two disks are not ready, voluntarily yielding thread\n ");  
-        SYSTEM_SCHEDULER->yield();
+void MirroredDisk::write(unsigned long _block_no, unsigned char *_buf)
+{
+  Console::puts("Issuing mirrored operation\n ");
+  issue_mirrored_operation(DISK_ID::MASTER, DISK_OPERATION::WRITE, _block_no); // IO command to the master
+  while (!master->is_ready())
+  {                                                    // Master disk is not ready for reading
+    SYSTEM_SCHEDULER->resume(Thread::CurrentThread()); // Add the thread to teh ready queue again
+    Console::puts("Master disk is not ready, voluntarily yielding thread\n ");
+    SYSTEM_SCHEDULER->yield();
   }
-  if(master->is_ready()){
-      //Master disk is ready, write to it , and wait for dependent disk
-      Console::puts("Master Disk is ready, write to it\n ");
-      int i; 
-      unsigned short tmpw;
-      for (i = 0; i < 256; i++) {
-        tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
-        Machine::outportw(0x1F0, tmpw);
-      }
-      while(!dependent->is_ready()){             //Both two disks are not ready for reading
-        SYSTEM_SCHEDULER->resume(Thread::CurrentThread());              //Add the thread to teh ready queue again
-        Console::puts("Dependent disk is not ready, voluntarily yielding thread\n ");  
-        SYSTEM_SCHEDULER->yield();
-      }
-      for (i = 0; i < 256; i++) {
-        tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
-        Machine::outportw(0x1F0, tmpw);
-      }
-      return;
+  // Master disk is ready, write to it , and wait for dependent disk
+  Console::puts("Master Disk is ready, write to it\n ");
+  int i;
+  unsigned short tmpw;
+  for (i = 0; i < 256; i++)
+  {
+    tmpw = _buf[2 * i] | (_buf[2 * i + 1] << 8);
+    Machine::outportw(0x1F0, tmpw);
   }
-  else{
-      //Dependent disk is ready, write to it , and wait for dependent disk
-      Console::puts("Dependent Disk is ready, write to it\n ");
-      int i; 
-      unsigned short tmpw;
-      for (i = 0; i < 256; i++) {
-        tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
-        Machine::outportw(0x1F0, tmpw);
-      }
-      while(!master->is_ready()){             //Both two disks are not ready for reading
-        SYSTEM_SCHEDULER->resume(Thread::CurrentThread());              //Add the thread to teh ready queue again
-        Console::puts("Dependent disk is not ready, voluntarily yielding thread\n ");  
-        SYSTEM_SCHEDULER->yield();
-      }
-      for (i = 0; i < 256; i++) {
-        tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
-        Machine::outportw(0x1F0, tmpw);
-      }
-      return;
+  issue_mirrored_operation(DISK_ID::DEPENDENT, DISK_OPERATION::WRITE, _block_no); // IO command to the dependent
+  while (!dependent->is_ready())
+  {                                                    // Dependent disk is not ready for reading
+    SYSTEM_SCHEDULER->resume(Thread::CurrentThread()); // Add the thread to teh ready queue again
+    Console::puts("Dependent disk is not ready, voluntarily yielding thread\n ");
+    SYSTEM_SCHEDULER->yield();
   }
-
-  
+  // Master disk is ready, write to it , and wait for dependent disk
+  Console::puts("Dependent Disk is ready, write to it\n ");
+  for (i = 0; i < 256; i++)
+  {
+    tmpw = _buf[2 * i] | (_buf[2 * i + 1] << 8);
+    Machine::outportw(0x1F0, tmpw);
+  }
 }
-
-// void MirroredDisk::wait_and_process()
-// {
-//   while(!master->is_ready()&&!dependent->is_ready())                  //Waits for both of the devices to be ready since it's a mirrored drive
-//   {
-//         SYSTEM_SCHEDULER->resume(Thread::CurrentThread());              //Add the thread to teh ready queue again
-//         Console::puts("Device is not ready, voluntarily yielding thread\n ");  
-//         SYSTEM_SCHEDULER->yield();
-//   }
-  
-//   Console::puts("Device is ready, performing the actual operation\n "); 
-//   unsigned long i;
-//   unsigned short tmpw;
-  
-//   /*Performing the actual data transfer*/
-//   if(request->op == DISK_OPERATION::READ)
-//   {
-// 	for (i = 0; i < 256; i++) 
-// 	{
-// 	tmpw = Machine::inportw(0x1F0);
-// 	request->buf[i*2]   = (unsigned char)tmpw;
-// 	request->buf[i*2+1] = (unsigned char)(tmpw >> 8);
-// 	}
-//   }
-//   else
-//   {
-//   	  for (i = 0; i < 256; i++) 
-//   	  {
-//     	  tmpw = request->buf[2*i] | (request->buf[2*i+1] << 8);
-//           Machine::outportw(0x1F0, tmpw);
-//   	  }
-// //   }
-  
-// }
